53404f057bcb90c0985c7b14f2c5d3a05c71b06a
Kasen Stephensen <kasenstephensen@gmail.com>
2022-11-28 14:46:18 -0800

    initial commit

---
+++ Constants/constants.py
@@ -0,0 +1,11 @@
+import consumer_key
+
+CONSUMER_KEY = consumer_key.CONSUMER_KEY
+
+THRESHOLD_DOWN_DAY = 0
+# Will only look at actions that give me a gain of 0.5% or more
+THRESHOLD_PERCENT_GAIN = 0.75
+# We will only consider premiums of $20 or more
+THRESHOLD_PREMIUM_GAIN = 20
+# How many years back do we want to get data for standard deviation
+YEARS_BACK_HISTORICAL = 2
